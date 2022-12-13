ec7a5b5b18400611f27585468d1454421201be29
Raniyah44 <rania.alotaibi@hotmail.com>
2022-12-13 10:14:03 +0300

    Initial Commit

---
+++ Signup/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Signup
+//
+//  Created by Raniyah Alotaibi on 19/05/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Signup/SignupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignupApp.swift
+//  Signup
+//
+//  Created by Raniyah Alotaibi on 19/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SignupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
